30d7e638494aa30728985493a895204acf9f4ddb
chylex <contact@chylex.com>
2022-07-07 20:03:58 +0200

    Fix accidentally breaking dynamic plugin unloading in IntelliJ

--- src/main/kotlin/com/chylex/intellij/disabletaskcontexts/PluginLoadListener.kt
+++ src/main/kotlin/com/chylex/intellij/disabletaskcontexts/PluginLoadListener.kt
@@ -6,6 +6,8 @@ import com.intellij.ide.plugins.IdeaPluginDescriptor
 
 class PluginLoadListener : DynamicPluginListener {
 	override fun checkUnloadPlugin(pluginDescriptor: IdeaPluginDescriptor) {
-		throw CannotUnloadPluginException("A restart is required to unload Disable Task Contexts plugin.")
+		if (pluginDescriptor.pluginId.idString == "com.chylex.intellij.disabletaskcontexts.DisableTaskContexts") {
+			throw CannotUnloadPluginException("A restart is required to unload Disable Task Contexts plugin.")
+		}
 	}
 }
